d71516035cc31ff421e8fd3ef0609fe9ffce8dd1
krxyzn <rafaibrahim1211@gmail.com>
2022-03-14 08:54:42 +0000

    santoni: remove SELINUX_NEVER_ALLOW
    Signed-off-by: krxyzn <rafaibrahim1211@gmail.com>
    Change-Id: Ie7797dc5606d38fe80e07640b0a4fb0e93d6d055

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,7 +182,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BUILD_BROKEN_TREBLE_SYSPROP_NEVERALLOW := true
 
