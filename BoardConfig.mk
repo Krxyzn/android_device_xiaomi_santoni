874b16ab6f53884904dcd6cfd9dcf809293a16af
Jabiyeff <cebiyevanar@gmail.com>
2022-03-18 18:06:22 +0400

    santoni: sepolicy: Import sepolicy-legacy-um source in device tree
    * Latest LOS policies breaks boot
    * Taken from (12.0 branch): https://github.com/crdroidandroid/android_device_qcom_sepolicy-legacy-um
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/xiaomi/santoni/sepolicy/legacy/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
